485aa137d214a10946601aad103065c8406000e8
Mohammad Reza Bayati <mrbayati2002@gmail.com>
2020-04-20 11:46:00 +0430

    done and work now :)

--- game_of_life/game.cpp
+++ game_of_life/game.cpp
@@ -4,30 +4,9 @@ using namespace std;
 
 const int w = 30;
 const int h = 30;
-const int xdir[8]={1, -1, -1, 1, 1, 0, -1, 0},
-            ydir[8]={1, -1, 1, -1, 0, 1, 0, -1};
-bool world[h][w];
-bool new_world[h][w];
 
-void zset() {
-    for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            new_world[y][x] = false;
-        }
-    }
-}
-bool check_stat(int lives, bool cell) {
-    if (cell) {
-        if (lives > 3 || lives < 2) {
-            return false;
-        }
-    }
-    else {
-        if (lives == 3) {
-            return true;
-        }
-    }
-}
+bool world[h][w];
+
 void draw() {
     cout << "\033[H";
     for (int y = 0; y < h; y++) {
@@ -45,58 +24,36 @@ void draw() {
 }
 
 void evolution() {
-    //zset();
-    /*new_world memset to zero */for (int y = 0; y < h; y++) {for (int x = 0; x < w; x++) {new_world[y][x] = world[y][x];}}
-    for (int y = 1; y < h - 1; y++) {
-        for (int x = 1; x < w - 1; x++) {
+    bool new_world[h][w];
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
             int lives = 0;
-            for (int q = 0; q < 8; q++) {
-                if (world[y + ydir[q]][x + xdir[q]]) {
-                    lives++;
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+                    if (world[(y + i) % h][(x + j) % w] != 0) {
+                        lives += 1;
+                    }
                 }
             }
-            new_world[y][x] = check_stat(lives, world[y][x]);
-            // new_world[y][x] = (lives == 3 || (lives == 2 && world[y][x]));
+            lives -= (world[y][x] ? 1 : 0);
+            if (world[y][x]) {
+                if (lives == 3 || lives == 2) {
+                    new_world[y][x] = true;
+                } else {
+                    new_world[y][x] = false;
+                }
+            }
+            else {
+                if (lives == 3) {
+                    new_world[y][x] = true;
+                } else {
+                    new_world[y][x] = false;
+                }
+
+            }
         }
     }
-    for (int i = 1; i < w - 1; i++) {
-        int lives = 0;
-        lives += world[0][i - 1] + world[0][i + 1] + world[1][i] + world[1][i - 1] + world[1][i + 1];
-        new_world[0][i] = check_stat(lives, world[0][i]);
-        // new_world[0][i] = (lives == 3 || (lives == 2 && world[0][i]));
-        lives = 0;
-        lives += world[h - 1][i - 1] + world[h - 1][i + 1] + world[h - 2][i] + world[h - 2][i - 1] + world[h - 2][i + 1];
-        new_world[h - 1][i] = check_stat(lives, world[h - 1][i]);
-        // new_world[h - 1][i] = (lives == 3 || (lives == 2 && world[h - 1][i]));
-    }
-    for (int i = 1; i < h - 1; i++) {
-        int lives = 0;
-        lives += world[i - 1][0] + world[i + 1][0] + world[i][1] + world[i - 1][1] + world[i + 1][1];
-        new_world[i][0] = check_stat(lives, world[i][0]);
-        // new_world[i][0] = (lives == 3 || (lives == 2 && world[i][0]));
-        lives = 0;
-        lives += world[i - 1][w - 1] + world[i + 1][w - 1] + world[i][w - 2] + world[i - 1][w - 2] + world[i + 1][w - 2];
-        new_world[i][w - 1] = check_stat(lives, world[i][w - 1]);
-        // new_world[i][w - 1] = (lives == 3 || (lives == 2 && world[i][w - 1]));
-    }
-    {
-        int lives = 0;
-        lives += world[0][1] + world[1][0] + world[1][1];
-        new_world[0][0] = check_stat(lives, world[0][0]);
-        // new_world[0][0] = (lives == 3 || (lives == 2 && world[0][0]));
-        lives = 0;
-        lives += world[0][w - 2] + world[1][w - 1] + world[1][w - 2];
-        new_world[0][w - 1] = check_stat(lives, world[0][w - 1]);
-        // new_world[0][w - 1] = (lives == 3 || (lives == 2 && world[0][w - 1]));
-        lives = 0;
-        lives += world[h - 2][0] + world[h - 2][1] + world[h - 1][1];
-        new_world[h - 1][0] = check_stat(lives, world[h - 1][0]);
-        // new_world[h - 1][0] = (lives == 3 || (lives == 2 && world[h - 1][0]));
-        lives = 0;
-        lives += world[h - 2][w - 1] + world[h - 2][w - 2] + world[h - 1][w - 2];
-        new_world[h - 1][w - 1] = check_stat(lives, world[h - 1][w - 1]);
-        // new_world[h - 1][w - 1] = (lives == 3 || (lives == 2 && world[h - 1][w - 1]));
-    }
+
     for (int y = 0; y < h; y++) {
         for (int x = 0; x < w; x++) {
             world[y][x] = new_world[y][x];
@@ -110,14 +67,14 @@ int main(){
     cout.tie(nullptr);
     srand(time(0));
     ////  choose random map or from a file
-    // cout << "Select your choose:\n1.Random map   2.From file        Choose: " << flush;
+    cout << "Select your choose:\n1.Random map   2.From file        Choose: " << flush;
     int choose = 1;
-    // cin >> choose;
+    cin >> choose;
     if (choose == 1) {     // random map
         for (int y = 0; y < h; y++) {
             for (int x = 0; x < w; x++) {
                 int this_rand = rand();
-                if (this_rand < RAND_MAX / 40) {
+                if (this_rand <= RAND_MAX / 10) {
                     world[y][x] = true;
                 }
             }
